9bf9d4495f17041be6a6d95f683aa19912b36154
id10937 <id10937@stu.aiswest.com>
2025-12-18 14:07:20 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1 style={{ fontSize: 32 }}>fundX</h1>
+      <p style={{ marginTop: 16 }}>
+        A social platform where startups pitch investors using 60-second videos.
+      </p>
+    </main>
+  )
+}
